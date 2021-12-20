28da953f46f5b773e0f66de85e16749097fe13f8
Brian Bockelman <bbockelman@morgridge.org>
2021-12-20 07:37:01 -0600

    Add tab completion to additional parameters.

--- src/ospool/cli/cli.py
+++ src/ospool/cli/cli.py
@@ -18,21 +18,60 @@ def ospool():
     Tools for querying the OSPool about pilot and payload jobs
     """
 
+
+def filter_obj_from_ctx(ctx):
+
+    return query.EntryFilter(gpus_only=ctx.params.get('gpus_only'),
+            resource=ctx.params.get('resource'), entry=ctx.params.get('entry_name'),
+            factory=ctx.params.get('factory'), ce_hostname=ctx.params.get('ce_hostname'))
+
+
 class EntryType(click.ParamType):
 
     def shell_complete(self, ctx, param, incomplete):
-        collector = htcondor.Collector(ctx.params['pool'])
-        entries = collector.query(ad_type=htcondor.AdTypes.Any,
-                        constraint='MyType =?= "glideresource"',
-                        projection=['GlideFactoryName'])
+
+        filter_obj = filter_obj_from_ctx(ctx)
+
         entry_names = set()
-        for entry in entries:
+        for entry in query.query_entries(ctx.params['pool'], filter_obj, []):
             if 'GlideFactoryName' in entry:
                 entry_names.add(entry['GlideFactoryName'].split("@")[0])
 
         entry_names = list(entry_names)
         entry_names.sort()
         return [click.shell_completion.CompletionItem(name) for name in entry_names if name.startswith(incomplete)]
+
+
+class ResourceType(click.ParamType):
+
+    def shell_complete(self, ctx, param, incomplete):
+
+        filter_obj = filter_obj_from_ctx(ctx)
+
+        resource_names = set()
+        for entry in query.query_entries(ctx.params['pool'], filter_obj, []):
+            if 'GLIDEIN_ResourceName' in entry:
+                resource_names.add(entry['GLIDEIN_ResourceName'])
+
+        resource_names = list(resource_names)
+        resource_names.sort()
+        return [click.shell_completion.CompletionItem(name) for name in resource_names if name.startswith(incomplete)]
+
+
+class CEHostnameType(click.ParamType):
+
+    def shell_complete(self, ctx, param, incomplete):
+
+        filter_obj = filter_obj_from_ctx(ctx)
+
+        ce_hostnames = set()
+        for entry in query.query_entries(ctx.params['pool'], filter_obj, []):
+            if 'GLIDEIN_Gatekeeper' in entry:
+                ce_hostnames.add(entry['GLIDEIN_Gatekeeper'].split(" ", 1)[0])
+
+        ce_hostnames = list(ce_hostnames)
+        ce_hostnames.sort()
+        return [click.shell_completion.CompletionItem(name) for name in ce_hostnames if name.startswith(incomplete)]
 
 
 class PoolType(click.ParamType):
@@ -81,8 +120,23 @@ def print_human_friendly_entry(entry, entries):
     if 'GLIDEIN_CPUS' in entries[0]:
         if entries[0]['GLIDEIN_CPUS'] == "auto":
             print("- Whole node entry " + ("with an estimated {} cores per glidein".format(entries[0]['GLIDEIN_ESTIMATED_CPUS']) if 'GLIDEIN_ESTIMATED_CPUS' in entries[0] else ''))
+        elif entries[0]['GLIDEIN_CPUS'] == "1":
+            print(f"- Entry has 1 core per glidein")
         else:
             print(f"- Entry has {entries[0]['GLIDEIN_CPUS']} cores per glidein")
+
+    if 'GLIDEIN_Resource_Slots' in entries[0]:
+        for slot_description in entries[0]['GLIDEIN_Resource_Slots'].split(";"):
+            slot_info = slot_description.split(",")
+            if slot_info[0] != 'GPUs':
+                continue
+            if len(slot_info) == 3:
+                if slot_info[1] == "1":
+                    print(f"- Entry has 1 GPU per glidein")
+                else:
+                    print(f"- Entry has {slot_info[1]} GPUs per glidein")
+            else:
+                print("- Entry has GPUs available to glidein")
 
     print()
 
@@ -131,12 +185,13 @@ def print_human_friendly_entry(entry, entries):
 @click.option("--output", default="human", help="Output formats")
 @click.option("--pool", default="flock.opensciencegrid.org", help="OSPool collector hostname.", type=PoolType(), show_default=True)
 @click.option("--factory", default="OSG", help="Name of OSG factory.", show_default=True, type=click.Choice(["OSG", "OSG-ITB"], case_sensitive=False))
-@click.option("--resource", help="Show only entries from resources matching glob.")
-@click.option("--ce-hostname", help="Show only entries from CE hostnames matching glob.")
+@click.option("--resource", help="Show only entries from resources matching glob.", type=ResourceType())
+@click.option("--ce-hostname", help="Show only entries from CE hostnames matching glob.", type=CEHostnameType())
+@click.option("--gpus-only", default=False, help="Only show resources with GPUs.", is_flag=True)
 @click.argument("entry_name", type=EntryType(), required=False)
-def show_pressure(pool, output, entry_name, factory, resource, ce_hostname):
+def show_pressure(pool, output, entry_name, factory, resource, ce_hostname, gpus_only):
 
-    filter_obj = query.EntryFilter(entry=entry_name, factory=factory, resource=resource, ce_hostname=ce_hostname)
+    filter_obj = query.EntryFilter(entry=entry_name, factory=factory, resource=resource, ce_hostname=ce_hostname, gpus_only=gpus_only)
 
     entry_info = collections.defaultdict(list)
     has_entry_name = entry_name is None
@@ -157,8 +212,8 @@ def show_pressure(pool, output, entry_name, factory, resource, ce_hostname):
 @click.command()
 @click.option("--pool", default="flock.opensciencegrid.org", help="OSPool collector hostname.", type=PoolType(), show_default=True)
 @click.option("--gpus-only", default=False, help="Only show resources with GPUs.", is_flag=True)
-@click.option("--resource", help="Show only entries from resources matching glob.")
-@click.option("--ce-hostname", help="Show only entries from CE hostnames matching glob.")
+@click.option("--resource", help="Show only entries from resources matching glob.", type=ResourceType())
+@click.option("--ce-hostname", help="Show only entries from CE hostnames matching glob.", type=CEHostnameType())
 @click.option("--factory", default="OSG", help="Name of OSG factory.", show_default=True, type=click.Choice(["OSG", "OSG-ITB"], case_sensitive=False))
 @click.argument("entry_name", type=EntryType(), required=False)
 def list_entries(pool, gpus_only, resource, factory, ce_hostname, entry_name):
